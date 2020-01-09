5e25bab27c78798db62d7edc2c35639a2b5a759c
Paulina Martinez <paulinisky@gmail.com>
2020-01-08 21:15:44 -0500

    Checkpoint 18 completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,28 +1,45 @@
 {
   $('button#play-pause').on('click', function() {
-    player.playPause();
+    helper.playPauseAndUpdate();
     $(this).attr('playState', player.playState);
   });
 
   $('button#next').on('click', function() {
-       if (player.playState !== 'playing') { return; }
+    if (player.playState !== 'playing') { return; }
 
-       const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-       const nextSongIndex = currentSongIndex + 1;
-       if (nextSongIndex >= album.songs.length) { return; }
+      const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+      const nextSongIndex = currentSongIndex + 1;
+      if (nextSongIndex >= album.songs.length) { return; }
 
-       const nextSong = album.songs[nextSongIndex];
-       player.playPause(nextSong);
+      const nextSong = album.songs[nextSongIndex];
+      helper.playPauseAndUpdate(nextSong);
      });
 
-     $('button#previous').on('click', function() {
-          if (player.playState !== 'playing') { return; }
+  $('button#previous').on('click', function() {
+      if (player.playState !== 'playing') { return; }
 
-          const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-          const nextSongIndex = currentSongIndex - 1;
-          if (nextSongIndex >= album.songs.length) { return; }
+      const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+      const nextSongIndex = currentSongIndex - 1;
+      if (nextSongIndex >= album.songs.length) { return; }
 
-          const nextSong = album.songs[nextSongIndex];
-          player.playPause(nextSong);
-        });
+      const nextSong = album.songs[nextSongIndex];
+      helper.playPauseAndUpdate(nextSong);
+      });
+
+  $('#volume-control input').on('input', function (event) {
+    player.setVolume(event.target.value);
+  });
+
+  $('#time-control input').on('input', function (event) {
+  player.skipTo(event.target.value);
+});
+
+  setInterval( () => {
+    if (player.playState !== 'playing') { return; }
+    const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime / duration) * 100;
+    $('#time-control .current-time').text( currentTime );
+    $('#time-control input').val(percent);
+  }, 1000);
 }
