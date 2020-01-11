7d40f69add1583bff358eb85011281748b2854bd
Paulina Martinez <paulinisky@gmail.com>
2020-01-10 23:49:38 -0500

    modify helper file

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,8 +1,8 @@
 class Helper{
   playPauseAndUpdate (song) {
     player.playPause(song);
-    const duration = player.getDuration();
-    $('#time-control .total-time').text( duration );
+    const time = player.currentlyPlaying.duration;
+    $('#time-control .total-time').text( time);
   }
 }
 
